c17a0526d9e5814336f043d5b9cda464f1315245
FunkyGary <gary80212@gmail.com>
2023-05-19 22:51:25 +0800

    feature/layout-done

--- src/App.js
+++ src/App.js
@@ -11,29 +11,41 @@ function App() {
     console.log("e.target.value", e.target.value);
   }
   return (
-    <div class="card text-center rounded">
-      <div class="card-header">你的一餐有多少碳排放呢？</div>
-      <div class="card-body">
-        {/* <h5 class="card-title">你的一餐有多少碳排放呢？</h5> */}
-        <p class="card-text">
-          <Form>
-            <Form.Group className="mb-3 d-flex align-items-center" controlId="formBasicCheckbox">
-              <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Preference</label>
-              <Form.Select aria-label="Default select example" onChange={selectEvent}>
-                <option>Open this select menu</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </Form.Select>
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </p>
-      </div>
-      <div class="card-footer text-muted">2 days ago</div>
-    </div>
+    <div className="w-100">
+      <div className="card text-center rounded mx-auto" style={{ width: '400px' }}>
+        <div className="card-header">你的一餐有多少碳排放呢？</div>
+        <div className="card-body">
+          {/* <h5 className="card-title">你的一餐有多少碳排放呢？</h5> */}
+          <p className="card-text">
+            <Form className="container">
+              <Form.Group className="mb-3 d-flex align-items-center row" controlId="formSelect">
+                <label className="my-1 mx-4 col" for="inlineFormCustomSelectPref">白米飯</label>
+                <Form.Select className="col" aria-label="Default select example" onChange={selectEvent}>
+                  <option>Open this select menu</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </Form.Select>
+              </Form.Group>
+              <Form.Group className="mb-3 d-flex align-items-center row" controlId="formSelect">
+                <label className="my-1 mx-4 col" for="inlineFormCustomSelectPref">白米飯</label>
+                <Form.Select className="col" aria-label="Default select example" onChange={selectEvent}>
+                  <option>Open this select menu</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </Form.Select>
+              </Form.Group>
+              <Button variant="primary" type="submit" className="w-100">
+                點我計算
+              </Button>
+            </Form>
+          </p>
+        </div>
+        {/* <div className="card-footer text-muted">2 days ago</div> */}
+      </div >
+    </div >
+
   );
 }
 
