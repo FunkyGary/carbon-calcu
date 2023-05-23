5ab237382de857821660bfc1370c67d5aa41013d
FunkyGary <gary80212@gmail.com>
2023-05-23 16:53:06 +0800

    feature/calcu-carbon-done

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ import React, { useState, useEffect } from 'react';
 //<iframe src="https://funkygary.github.io/carbon-calcu/" style="width: 100%; height: 100%"></iframe>
 
 function App() {
+  const [riceCarbon, setRiceCarbon] = useState(0.12)
+  const [carbCarbon, setCarbCarbon] = useState(0.124)
+  const [meat1Carbon, setMeat1Carbon] = useState(15.5)
+  const [meat2Carbon, setMeat2Carbon] = useState(15.5)
+  const [vetge1Carbon, setVetge1Carbon] = useState(0.06)
+  const [vetge2Carbon, setVetge2Carbon] = useState(0.06)
+  const [drinkCarbon, setDrinkCarbon] = useState(0.32)
+  const [totalCarbo, setTotalCarbon] = useState(0)
+  console.log(totalCarbo)
 
-  function selectEvent(e) {
-    console.log("e.target.value", e.target.value);
-  }
   return (
     <div className="card text-center rounded mx-auto" style={{ width: '400px' }}>
       <div className="card-header" style={{ 'background-color': '#00a9e0', color: 'white' }}>你的一餐有多少碳排放呢？</div>
@@ -19,68 +25,99 @@ function App() {
           <Form className="container">
             <Form.Group className="mb-3 d-flex align-items-center row" controlId="formSelect">
               <label className="my-1 mx-4 col" for="inlineFormCustomSelectPref">米飯 🍚</label>
-              <Form.Select className="col" aria-label="Default select example" onChange={selectEvent}>
-                <option value="0.1759">1碗 160公克</option>
-                <option value="0.3518">2碗 320公克</option>
-                <option value="0.5277">3碗 480公克</option>
-                <option value="0.7036">4碗 640公克</option>
-                <option value="0.8795">5碗 800公克</option>
-                <option value="1.0554">6碗 960公克</option>
+              <Form.Select className="col" aria-label="Default select example" onChange={e => setRiceCarbon(e.target.value)} value={riceCarbon}>
+                <option value={0.12}>1碗 75公克</option>
+                <option value={0.24}>2碗 150公克</option>
+                <option value={0.36}>3碗 225公克</option>
+                <option value={0.48}>4碗 300公克</option>
+                <option value={0.6}>5碗 375公克</option>
+                <option value={0.72}>6碗 450公克</option>
               </Form.Select>
             </Form.Group>
             <Form.Group className="mb-3 d-flex align-items-center row" controlId="formSelect">
-              <label className="my-1 mx-4 col" for="inlineFormCustomSelectPref">麵食 🍜</label>
-              <Form.Select className="col" aria-label="Default select example" onChange={selectEvent}>
-                <option value="0.124">1碗 100公克</option>
-                <option value="0.248">2碗 200公克</option>
-                <option value="0.372">3碗 300公克</option>
-                <option value="0.496">4碗 400公克</option>
-                <option value="0.62">5碗 500公克</option>
-                <option value="0.744">6碗 600公克</option>
+              <label className="my-1 mx-4 col" for="inlineFormCustomSelectPref">其它澱粉來源 🍜</label>
+              <Form.Select className="col" aria-label="Default select example" onChange={e => setCarbCarbon(e.target.value)} value={carbCarbon}>
+                <option value={0.124}>1碗 10公克</option>
+                <option value={0.25}>2碗 200公克</option>
+                <option value={0.37}>3碗 300公克</option>
+                <option value={0.48}>4碗 400公克</option>
+                <option value={0.1}>馬鈴薯2顆 200公克</option>
+                <option value={0.3}>玉米1根 100公克</option>
+                <option value={0.2}>裸麥吐司4片 120公克</option>
               </Form.Select>
             </Form.Group>
             <Form.Group className="mb-3 d-flex align-items-center row" controlId="formSelect">
-              <label className="my-1 mx-4 col" for="inlineFormCustomSelectPref">豆魚蛋肉類 🍗</label>
-              <Form.Select className="col" aria-label="Default select example" onChange={selectEvent}>
-                <option value="15.5">牛肉 100公克</option>
-                <option value="2.4">豬肉 100公克</option>
-                <option value="1.82">雞肉 100公克</option>
-                <option value="1.34">魚肉 100公克</option>
-                <option value="0.93">蝦子 100公克</option>
-                <option value="0.08">豆腐 100公克</option>
-                <option value="0.05">堅果 100公克</option>
-                <option value="0.16">雞蛋 1顆</option>
+              <label className="my-1 mx-4 col" for="inlineFormCustomSelectPref">豆魚蛋肉類一 🍗</label>
+              <Form.Select className="col" aria-label="Default select example" onChange={e => setMeat1Carbon(e.target.value)} value={meat1Carbon}>
+                <option value={15.5}>牛肉 100公克</option>
+                <option value={2.4}>豬肉 100公克</option>
+                <option value={1.82}>雞肉 100公克</option>
+                <option value={1.34}>魚肉 100公克</option>
+                <option value={0.93}>蝦子 100公克</option>
+                <option value={0.08}>豆腐 100公克</option>
+                <option value={0.05}>堅果 100公克</option>
+                <option value={0.16}>雞蛋 1顆</option>
               </Form.Select>
             </Form.Group>
             <Form.Group className="mb-3 d-flex align-items-center row" controlId="formSelect">
-              <label className="my-1 mx-4 col" for="inlineFormCustomSelectPref">蔬菜水果類 🍎</label>
-              <Form.Select className="col" aria-label="Default select example" onChange={selectEvent}>
-                <option value="0.06">蘋果 1顆</option>
-                <option value="0.11">香蕉 1根</option>
-                <option value="0.05">橘子 1顆</option>
-                <option value="0.22">莓果 144公克</option>
-                <option value="0.05">甘藍菜 100公克</option>
-                <option value="0.04">根莖類 100公克</option>
-                <option value="0.32">番茄 150公克</option>
+              <label className="my-1 mx-4 col" for="inlineFormCustomSelectPref">豆魚蛋肉類二 🍤</label>
+              <Form.Select className="col" aria-label="Default select example" onChange={e => setMeat2Carbon(e.target.value)} value={meat2Carbon}>
+                <option value={15.5}>牛肉 100公克</option>
+                <option value={2.4}>豬肉 100公克</option>
+                <option value={1.82}>雞肉 100公克</option>
+                <option value={1.34}>魚肉 100公克</option>
+                <option value={0.93}>蝦子 100公克</option>
+                <option value={0.08}>豆腐 100公克</option>
+                <option value={0.05}>堅果 100公克</option>
+                <option value={0.16}>雞蛋 1顆</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className="mb-3 d-flex align-items-center row" controlId="formSelect">
+              <label className="my-1 mx-4 col" for="inlineFormCustomSelectPref">蔬菜水果類ㄧ 🍎</label>
+              <Form.Select className="col" aria-label="Default select example" onChange={e => setVetge1Carbon(e.target.value)} value={vetge1Carbon}>
+                <option value={0.06}>蘋果 1顆</option>
+                <option value={0.11}>香蕉 1根</option>
+                <option value={0.05}>橘子 1顆</option>
+                <option value={0.22}>莓果 144公克</option>
+                <option value={0.05}>甘藍菜 100公克</option>
+                <option value={0.04}>根莖類 100公克</option>
+                <option value={0.32}>番茄 150公克</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className="mb-3 d-flex align-items-center row" controlId="formSelect">
+              <label className="my-1 mx-4 col" for="inlineFormCustomSelectPref">蔬菜水果類二 🍌</label>
+              <Form.Select className="col" aria-label="Default select example" onChange={e => setVetge2Carbon(e.target.value)} value={vetge2Carbon}>
+                <option value={0.06}>蘋果 1顆</option>
+                <option value={0.11}>香蕉 1根</option>
+                <option value={0.05}>橘子 1顆</option>
+                <option value={0.22}>莓果 144公克</option>
+                <option value={0.05}>甘藍菜 100公克</option>
+                <option value={0.04}>根莖類 100公克</option>
+                <option value={0.32}>番茄 150公克</option>
               </Form.Select>
             </Form.Group>
             <Form.Group className="mb-3 d-flex align-items-center row" controlId="formSelect">
               <label className="my-1 mx-4 col" for="inlineFormCustomSelectPref">點心飲料 ☕</label>
-              <Form.Select className="col" aria-label="Default select example" onChange={selectEvent}>
-                <option value="0.6">牛奶1杯 250公克</option>
-                <option value="0.3">米漿1杯 250公克</option>
-                <option value="0.25">豆漿1杯 250公克</option>
-                <option value="0.4">咖啡1杯 15公克咖啡豆</option>
-                <option value="0.19">鳳梨酥1顆 35公克</option>
-                <option value="0.95">黑巧克力1片 50公克</option>
-                <option value="0.17">可樂1罐 330毫升</option>
-                <option value="0.22">啤酒1罐 330毫升</option>
-                <option value="0.13">紅酒1杯 150毫升</option>
-                <option value="0.12">紅茶1份 鋁箔包300毫升</option>
-                <option value="0.14">奶茶1份 鋁箔包300毫升</option>
+              <Form.Select className="col" aria-label="Default select example" onChange={e => setDrinkCarbon(e.target.value)} value={drinkCarbon}>
+                <option value={0.6}>牛奶1杯 250公克</option>
+                <option value={0.3}>米漿1杯 250公克</option>
+                <option value={0.25}>豆漿1杯 250公克</option>
+                <option value={0.48}>優酪乳1份 250公克</option>
+                <option value={0.4}>咖啡1杯 15公克咖啡豆</option>
+                <option value={0.12}>紅茶1份 300毫升</option>
+                <option value={0.14}>奶茶1份 300毫升</option>
+                <option value={0.19}>鳳梨酥1顆 35公克</option>
+                <option value={0.95}>黑巧克力1片 50公克</option>
+                <option value={0.17}>花生1包 250公克</option>
+                <option value={0.17}>可樂1罐 330毫升</option>
+                <option value={0.22}>啤酒1罐 330毫升</option>
+                <option value={0.13}>紅酒1杯 150毫升</option>
               </Form.Select>
             </Form.Group>
-            <Button variant="primary" className="w-100 border-0" style={{ 'background-color': '#ed8c01', color: 'white' }}>
+            <Button variant="primary" className="w-100 border-0" style={{ 'background-color': '#ed8c01', color: 'white' }} onClick={() => {
+              let total = riceCarbon * 1 + meat2Carbon * 1 + meat1Carbon * 1 + carbCarbon * 1 + drinkCarbon * 1 + vetge2Carbon * 1 + vetge1Carbon * 1
+              setTotalCarbon(Math.round(total * 100) / 100)
+            }}>
               點我計算
             </Button>
           </Form>
